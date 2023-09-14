2abc706a37fb078200aa093b9a6a2ab610900200
Jose Fernandez <jotaj1019@gmail.com>
2023-09-13 20:19:46 -0600

    twenty third commit: removing 'create_env_file' from github actions

--- src/components/mapbox/index.tsx
+++ src/components/mapbox/index.tsx
@@ -21,8 +21,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 
-console.log({mapBoxToken});
-
 type MapBoxSearchProps = {
     onChange?: (any) => any;
 };
